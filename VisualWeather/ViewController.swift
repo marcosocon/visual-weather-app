4ed83c93273067871050aa7ecea59b310a3cd9db
marcosocon <oconmarcos97@gmail.com>
2020-04-24 20:07:55 -0300

    added GeoDecoding for friendly city labels and icons

--- VisualWeather/ViewController.swift
+++ VisualWeather/ViewController.swift
@@ -15,35 +15,50 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var dayLabel: UILabel!
     @IBOutlet weak var conditionLabel: UILabel!
     @IBOutlet weak var temperatureLabel: UILabel!
+    var soon = Date()
 
     var locationManager: CLLocationManager?
     override func viewDidLoad() {
         super.viewDidLoad()
-
         //location manager setup
         locationManager = CLLocationManager()
-        locationManager?.delegate = self
         locationManager?.requestWhenInUseAuthorization()
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager?.delegate = self
+            locationManager?.startUpdatingLocation()
+        }
     }
     
     //function to handle when the user has opted in or out of our location request.
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         switch status {
-    
-        case .notDetermined:
-            print("notDetermined")        // location permission not asked for yet
         case .authorizedWhenInUse:
             print("authorizedWhenInUse")
-            locationLabel.text = "Thanks for authorizing our app to use your location!"
-        case .authorizedAlways:
-            print("authorizedAlways")     // location authorized
-        case .restricted:
-            print("restricted")           // TODO: handle
         case .denied:
-            print("denied")
-            locationLabel.text = "Our app cannot function properly without location permissions"
+            print("asdasds")
+        case .notDetermined:
+            print("notDetermined")
         @unknown default:
-            print("unable to handle this scenario")
+                print("unable to handle this scenario")
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            if location.timestamp > soon {
+                let geocoder = CLGeocoder()
+                geocoder
+                    .reverseGeocodeLocation(
+                        location,
+                        completionHandler: { (placemarks, error) in
+                            if error == nil {
+                                let firstLocation = placemarks?[0]
+                                self.locationLabel.text = "\(firstLocation?.locality ?? ""), \(firstLocation?.country ?? "")"
+                            }
+                })
+                // set next call in 30'
+                soon = Date().addingTimeInterval(1800)
+            }
         }
     }
 }
