c949997cd42baa9ee52d7954843da0ad33b9aef4
marcosocon <oconmarcos97@gmail.com>
2020-04-24 11:00:39 -0300

    added base code

--- VisualWeather/ViewController.swift
+++ VisualWeather/ViewController.swift
@@ -7,14 +7,44 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var conditionLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
 
+    var locationManager: CLLocationManager?
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        //location manager setup
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.requestWhenInUseAuthorization()
     }
-
-
+    
+    //function to handle when the user has opted in or out of our location request.
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+    
+        case .notDetermined:
+            print("notDetermined")        // location permission not asked for yet
+        case .authorizedWhenInUse:
+            print("authorizedWhenInUse")
+            locationLabel.text = "Thanks for authorizing our app to use your location!"
+        case .authorizedAlways:
+            print("authorizedAlways")     // location authorized
+        case .restricted:
+            print("restricted")           // TODO: handle
+        case .denied:
+            print("denied")
+            locationLabel.text = "Our app cannot function properly without location permissions"
+        @unknown default:
+            print("unable to handle this scenario")
+        }
+    }
 }
 
